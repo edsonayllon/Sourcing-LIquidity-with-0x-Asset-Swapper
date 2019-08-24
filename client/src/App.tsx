7664ac841bd82fbfe93be74006bb159dfa6d23dc
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-08-24 02:34:07 +0000

    working asset swap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,13 @@ import { SwapQuoter } from '@0x/asset-swapper';
 import { BigNumber } from '0x.js';
 const Web3 = require('web3');
 
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'https://api.relayer.com/v2';
-const daiTokenAddress = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359';
-const wethTokenAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-
-const web3 = new Web3(Web3.givenProvider);
-
 declare let window: any;
+const web3 = new Web3(Web3.givenProvider);
+const apiUrl = 'https://api.radarrelay.com/0x/v2/';
+const tokenAddresses = {
+  DAI: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  WETH: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+}
 
 const App: React.FC = () => {
 
@@ -25,18 +24,22 @@ const App: React.FC = () => {
       // console.log(sell)
       window.ethereum.enable();
 
+      let wei = web3.utils.fromWei('3', 'ether');
+
+      let makerTokenAddress = tokenAddresses['DAI'];
+      let takerTokenAddress = tokenAddresses['WETH'];
+      let makerAssetBuyAmount = new BigNumber(200000);
+
       const quoter = SwapQuoter.getSwapQuoterForStandardRelayerAPIUrl(
         web3.currentProvider,
-        apiUrl
+        apiUrl,
+        {
+          networkId: 1,
+          orderRefreshIntervalMs: 500,
+        }
       );
 
       console.log(quoter);
-
-      let wei = web3.utils.fromWei('3', 'ether');
-
-      let makerTokenAddress = daiTokenAddress;
-      let takerTokenAddress = wethTokenAddress;
-      let makerAssetBuyAmount = new BigNumber(200000);
 
       // Get a quote to buy three units of DAI
       const quote = await quoter.getMarketBuySwapQuoteAsync(
